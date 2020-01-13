775881789c2d923e46c261cb61787d15f9930c08
kimjino1996 <jino1996@naver.com>
2020-01-13 10:57:35 +0900

    치킨배달수정

--- baekjoon/15686. 치킨배달/진오/chiken_distance.cpp
+++ baekjoon/15686. 치킨배달/진오/chiken_distance.cpp
@@ -3,7 +3,7 @@
 #include <algorithm>
 #include <cstdlib>
 #include <queue>
-#define MAX 999
+#define MAX 9999
 using namespace std;
 
 struct location {
@@ -11,18 +11,18 @@ struct location {
 	int col;
 };
 bool CompareLocation(const location &x, const location &y);
-void Comb( int now, int count, vector<location> chicken, vector <location> combo);
+void Comb(int now, int count, vector<location> chicken, vector <location> combo);
 int BruteForce_dist(vector<location> combo);
 int min_distance(int row, int col, vector<location>combo);
 
 int max_chick;
-int arr[50][50];
-int min_dist=MAX;
+int arr[51][51];
+int min_dist = MAX;
 vector<location> min_combo;
-int arr_sized=0;
+int arr_sized = 0;
 
 int main() {
-	
+
 
 	vector < location> chicken;
 	location temp;
@@ -43,8 +43,8 @@ int main() {
 		}
 	}
 	sort(chicken.begin(), chicken.end(), CompareLocation);
-	Comb(-1,0, chicken,combo);
-	cout << " min value : " << min_dist;
+	Comb(-1, 0, chicken, combo);
+	cout << min_dist;
 }
 bool CompareLocation(const location &x, const location &y) {
 	if (x.row == y.row) {
@@ -53,25 +53,25 @@ bool CompareLocation(const location &x, const location &y) {
 	else
 		return x.row < y.row;
 }
-void Comb(int now,int count,vector<location> chicken,vector <location> combo) { // now 는 지금 까지뽑은 배열위치, count 는 몇개 뽑았는지, chicken 은 현재 좌표가 들어가있는 곳, 조합된 좌표를 위해 combi 라는 vector 를 하나 더 만듬.
-	
+void Comb(int now, int count, vector<location> chicken, vector <location> combo) { // now 는 지금 까지뽑은 배열위치, count 는 몇개 뽑았는지, chicken 은 현재 좌표가 들어가있는 곳, 조합된 좌표를 위해 combi 라는 vector 를 하나 더 만듬.
+
 	if (count == max_chick) {
-		for (auto it = combo.begin(); it != combo.end(); it++) {
-			cout << "(" << (*it).row << ",";
-			cout << (*it).col << ") ";
+		/*		for (auto it = combo.begin(); it != combo.end(); it++) {
+		cout << "(" << (*it).row << ",";
+		cout << (*it).col << ") ";
 		}
-		cout << "\n";
-		if (min_dist > BruteForce_dist(combo)){
+		cout << "\n";*/
+		if (min_dist > BruteForce_dist(combo)) {
 			min_dist = BruteForce_dist(combo);
 			min_combo = combo;
 		}
 		return;
 	}
-	for (int i = now+1; i < chicken.size(); i++) {
-			combo.push_back(chicken[i]); // 일단 이자리를 대상으로 조합을 구할거다!
-			Comb(i, count + 1, chicken, combo); //다음 자리로 넘어간다.
-			combo.pop_back();
-			count = combo.size();
+	for (int i = now + 1; i < chicken.size(); i++) {
+		combo.push_back(chicken[i]); // 일단 이자리를 대상으로 조합을 구할거다!
+		Comb(i, count + 1, chicken, combo); //다음 자리로 넘어간다.
+		combo.pop_back();
+		count = combo.size();
 	}
 }
 int BruteForce_dist(vector<location> combo) {
