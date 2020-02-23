22e7999125269689fc458cbd0fd24aa8cbf62942
kimjino1996 <jino1996@naver.com>
2020-02-23 23:13:42 +0900

    먼저 2개

---
+++ baekjoon/14890. 경사로/진오/slope.cpp
@@ -0,0 +1,200 @@
+﻿#include <iostream>
+
+#define MAX 101
+using namespace std;
+
+int arr_siz;
+int length;
+int able_line = 0;
+int arr[MAX][MAX];
+int visit[MAX][MAX] = { 0, };
+void check_line_row(int col);
+int down_check_row(int row, int col);
+int up_check_row(int row, int col);
+void check_line_col(int row);
+int down_check_col(int row, int col);
+int up_check_col(int row, int col);
+void visit_init();
+int main() {
+
+	cin >> arr_siz;
+	cin >> length;
+	for (int i = 0; i < arr_siz; i++) {
+		for (int j = 0; j < arr_siz; j++) {
+			cin>>arr[i][j];
+		}
+	}
+	for (int i = 0; i < arr_siz; i++) {
+		check_line_row(i);
+	}
+	visit_init();
+	for (int i = 0; i < arr_siz; i++) {
+		check_line_col(i);
+	}
+	cout << able_line;
+}
+void visit_init() {
+	for (int i = 0; i < arr_siz; i++) {
+		for (int j = 0; j < arr_siz; j++) {
+			visit[i][j] = 0;
+		}
+	}
+}
+void check_line_row(int col) {
+	int temp;
+	int i;
+
+	for (i = 0; i < arr_siz-1; i++) {
+		if (arr[i][col] < arr[i + 1][col]) { //커지는것
+			temp = up_check_row(i, col);
+			if (temp != 0) {
+				break;
+			}
+		}
+		else if (arr[i][col] > arr[i + 1][col]) { // 작아지는것
+			temp = down_check_row(i, col);
+			if (temp != 0) {
+				break;
+			}
+		}
+	}
+	if (i == arr_siz - 1) {
+		able_line++;
+	}
+	//arr_siz-1 인지아닌지 확인하면 됨
+}
+int down_check_row(int row, int col) {// 밑으로 내려가는거 체크
+	int flag = 0; // 옳은 상황
+
+	if (row + length >= arr_siz) {
+		flag = 1;
+		return flag;
+	}
+	// 여기서 length++ 얼만큼 될지모르기때문에 그전에 미리 검사
+
+	for (int i = 1; i <= length; i++) {
+		
+		if (arr[row][col] - 1 != arr[row + i][col]||visit[row+i][col]==1) {
+			flag = 1;
+			break;
+		}
+	}
+	if (flag == 0) {
+		for (int i = 1; i <= length; i++) {
+			visit[row + i][col] = 1;
+		}
+	}
+	return flag;
+
+}
+int up_check_row(int row, int col) {
+	int flag = 0;
+
+	//row +1 은 검사할 필요 없음 애초에 row -1 까지만 간다
+	if (arr[row + 1][col] != arr[row][col]+1) {
+		flag = 1;
+		return flag;
+	}
+	//현재자리에 경사로가 놓아져 있는지
+	if (visit[row][col] == 1) {
+		flag = 1;
+		return flag;
+	}
+	//여기서 length ++ 검사
+	if (row - length+1< 0) {
+		flag = 1;
+		return flag;
+	}
+
+	for (int i = 1; i < length; i++) {
+		if (arr[row][col] != arr[row - i][col] || visit[row-i][col] == 1) {
+			flag = 1;
+			break;
+		}
+	}
+	if (flag == 0) {
+		for (int i = 0; i < length; i++) {
+			visit[row-i][col] = 1;
+		}
+	}
+	return flag;
+}
+
+void check_line_col(int row) {
+	int temp;
+	int i;
+
+	for (i = 0; i < arr_siz - 1; i++) {
+		if (arr[row][i] < arr[row][i+1]) { //커지는것
+			temp = up_check_col(row, i);
+			if (temp != 0) {
+				break;
+			}
+		}
+		else if (arr[row][i] > arr[row][i+1]) { // 작아지는것
+			temp = down_check_col(row, i);
+			if (temp != 0) {
+				break;
+			}
+		}
+	}
+	if (i == arr_siz - 1) {
+		able_line++;
+	}
+	//arr_siz-1 인지아닌지 확인하면 됨
+}
+int down_check_col(int row, int col) {// 밑으로 내려가는거 체크
+	int flag = 0; // 옳은 상황
+
+	if (col + length >= arr_siz) {
+		flag = 1;
+		return flag;
+	}
+	// 여기서 length++ 얼만큼 될지모르기때문에 그전에 미리 검사
+
+	for (int i = 1; i <= length; i++) {
+
+		if (arr[row][col] - 1 != arr[row][col+i]||visit[row][col+i]) {
+			flag = 1;
+			break;
+		}
+	}
+	if (flag == 0) {
+		for (int i = 1; i <= length; i++) {
+			visit[row][col+i] = 1;
+		}
+	}
+	return flag;
+
+}
+int up_check_col(int row, int col) {
+	int flag = 0;
+
+	//row +1 은 검사할 필요 없음 애초에 row -1 까지만 간다
+	if (arr[row][col+1] != arr[row][col] + 1) {
+		flag = 1;
+		return flag;
+	}
+	//여기서 경사로 놓았는지검사
+	if (visit[row][col] == 1) {
+		flag = 1;
+		return flag;
+	}
+	//여기서 length ++ 검사
+	if (col - length + 1< 0) {
+		flag = 1;
+		return flag;
+	}
+	for (int i = 1; i < length; i++) {
+		if (arr[row][col] != arr[row][col-i]||visit[row][col-i]==1) {
+			flag = 1;
+			break;
+		}
+	}
+	if (flag == 0) {
+		for (int i = 0; i < length; i++) {
+			visit[row][col-i] = 1;
+		}
+	}
+	return flag;
+}
---
+++ baekjoon/17142. 연구소3/진오/virus.cpp
@@ -0,0 +1,158 @@
+﻿#include <iostream>
+#include <vector>
+#include <queue>
+#define MAX 51
+#define MAX_VALUE 999999
+using namespace std;
+
+
+struct location{
+	int row;
+	int col;
+};
+
+int active_virus;
+int something=0;
+int min_time= MAX_VALUE;
+int flag = 0; // 칸이 가득 꽉찼나?
+
+int arr[MAX][MAX] = { 0, };
+int visit[MAX][MAX] = { 0, };
+int arr_siz;
+void Comb(int now, int count, vector<location> virus, vector <location> combo);
+int bfs(vector <location> combo);
+void initialized_visit();
+void ClearQueue(queue<location> &someQueue);
+int main() {
+	vector < location> virus;
+	location temp;
+	vector < location> combo;
+
+	cin >> arr_siz;
+	cin >> active_virus;
+
+	for (int i = 0; i < arr_siz; i++) {
+		for (int j = 0; j < arr_siz; j++) {
+			cin>>arr[i][j];
+			if (arr[i][j] >0) {
+				something++;// 벽이나 비활성화 virus
+				if (arr[i][j] == 2) {
+					temp.row = i;
+					temp.col = j;
+					virus.push_back(temp);
+				}
+			}
+		}
+	}
+	Comb(-1,0,virus,combo);
+	if (min_time == MAX_VALUE) {
+		cout << -1;
+	}
+	else 
+		cout << min_time;
+}
+
+void Comb(int now, int count, vector<location> virus, vector <location> combo) {
+
+	if (count == active_virus) {
+		if (min_time> bfs(combo)) {
+			min_time = bfs(combo);
+		}
+		return;
+	}
+	for (int i = now + 1; i < virus.size(); i++) {
+		combo.push_back(virus[i]); // 일단 이자리를 대상으로 조합을 구할거다!
+		Comb(i, count + 1, virus, combo); //다음 자리로 넘어간다.
+		combo.pop_back();
+		count = combo.size();
+	}
+}
+int bfs(vector <location> combo) {
+	queue <location> q;
+	queue <location> temp_q;
+
+	location a;
+	location temp;
+	int time=0;
+
+	while (!combo.empty()) {
+		q.push(combo.back());
+		combo.pop_back();
+	}
+	initialized_visit();
+	while (!q.empty()) {
+		if (flag == arr_siz * arr_siz) {
+			return time;
+		}
+		temp_q = q;
+		ClearQueue(q);
+		while (!temp_q.empty()) {
+			if (flag == arr_siz * arr_siz) {
+				break;
+			}
+			temp = temp_q.front();
+			temp_q.pop();
+			visit[temp.row][temp.col] = 1;
+			//1 ,2 ,0 일때 어떻게 갈지 정해야되고 count 세는거 해놓아야함. 
+			a.row = temp.row + 1;
+			a.col = temp.col;
+			if (a.row < arr_siz&&visit[a.row][a.col] != 1) {
+				q.push(a);
+				if (visit[a.row][a.col] != 2) {
+					flag++;
+				}
+				visit[a.row][a.col] = 1;
+			}
+			a.row = temp.row - 1;
+			a.col = temp.col;
+			if (a.row >= 0 && visit[a.row][a.col] != 1) {
+				q.push(a);
+				if (visit[a.row][a.col] != 2) {
+					flag++;
+				}
+				visit[a.row][a.col] = 1;
+			}
+			a.row = temp.row;
+			a.col = temp.col + 1;
+			if (a.col < arr_siz&&visit[a.row][a.col] != 1) {
+				q.push(a);
+				if (visit[a.row][a.col] != 2) {
+					flag++;
+				}
+				visit[a.row][a.col] = 1;
+				
+			}
+			a.row = temp.row;
+			a.col = temp.col - 1;
+			if (a.col >= 0 && visit[a.row][a.col] != 1) {
+				q.push(a);
+				if (visit[a.row][a.col] != 2) {
+					flag++;
+				}
+				visit[a.row][a.col] = 1;
+			}
+		}
+		time++;
+	}
+
+	if (flag == arr_siz * arr_siz) {
+		return time;
+	}
+	else
+		MAX_VALUE;
+}
+void initialized_visit() {
+	for (int i = 0; i < MAX; i++) {
+		for (int j = 0; j < MAX; j++) {
+			visit[i][j] = arr[i][j];
+		}
+	}
+	flag = 0;
+	flag = something; // 이바이러스 까진 안가도 됨
+}
+
+void ClearQueue(queue<location> &someQueue)
+{
+	queue<location> empty;
+	someQueue=empty;
+}
