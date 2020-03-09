a0c7fe45a04323b76b209c39a7e3739fb8ac2a79
kimjino1996 <jino1996@naver.com>
2020-03-09 18:57:02 +0900

    문제까지 같이올려용

---
+++ baekjoon/13460. 구슬탈출2/진오/bid.cpp
@@ -0,0 +1,347 @@
+﻿#include <iostream>
+#include <vector>
+
+#define MAX 11
+using namespace std;
+
+struct state {
+	int red_row;
+	int red_col;
+	int blue_row;
+	int blue_col;
+	int dir; //1 오른쪽 2 밑 3 왼쪽 4 위
+};
+struct direction {
+	int row;
+	int col;
+};
+
+
+
+int flag = 0; // 1되면 끝
+int push_row;
+int push_col;
+vector <state> state_arr; //cur_state 쌓기
+direction a[5]; // dir 기반 방향찾기
+
+
+int row_siz;
+int col_siz;
+int arr[MAX][MAX]; // 구슬은 2으로 받고, 벽은 1로받자, 움직이는건 0 구멍은 3
+state cur_state;
+
+void move();
+int go_right(int row, int col, int dir);
+int go_down(int row, int col, int dir);
+int go_left(int row, int col, int dir);
+int go_up(int row, int col, int dir);
+void initial_direction();
+
+int main() {
+	char temp;
+	int time = 0;
+	cin >> row_siz;
+	cin >> col_siz;
+
+	for (int i = 0; i < row_siz; i++) {
+		for (int j = 0; j < col_siz; j++) {
+			cin >> temp;
+			if (temp == '#') {
+				arr[i][j] = 1;
+			}
+			else if (temp == '.') {
+				arr[i][j] = 0;
+			}
+			else if (temp == 'O') {
+				arr[i][j] = 3;
+			}
+			else if (temp == 'B') {
+				arr[i][j] = 2;
+				cur_state.blue_row = i;
+				cur_state.blue_col = j;
+			}
+			else if (temp == 'R') {
+				arr[i][j] = 2;
+				cur_state.red_row = i;
+				cur_state.red_col = j;
+			}
+		}
+	}
+	cur_state.dir = 1;// cur state 초기화 완료
+	initial_direction();
+	move();
+	time = state_arr.size();
+	cout << time;
+	return 0;
+}
+
+ void move() {
+	state temp;
+	state push_temp;// 넣을거
+	int count = 0; //움직였는지
+	int vector_find = 0;
+	state_arr.push_back(cur_state);
+	
+	while (!state_arr.empty()) {
+		
+		temp=state_arr.back();
+		state_arr.pop_back(); //꺼내고
+		//////////////////////////////////여기서 방향보고 합시다 4까지인지 검사하기
+		push_temp = temp;
+		if (temp.dir < 4) {
+			push_temp.dir = push_temp.dir + 1; //다시 dir +1 해서 넣어놓기
+			//state_arr.push_back(push_temp);
+			for (int i = 0; i < state_arr.size(); i++) {
+				if (state_arr[i].blue_row == push_temp.blue_row&&state_arr[i].blue_col == push_temp.blue_col&&state_arr[i].red_row == push_temp.red_row&&state_arr[i].red_col == push_temp.red_col) {
+					vector_find = 1;
+				}
+				else {
+
+				}
+			}
+			if (vector_find == 0) {
+				state_arr.push_back(push_temp);
+			}
+		}
+		vector_find = 0;
+		switch (temp.dir) {
+		case 1: // 얘가 오른쪽
+			if (temp.blue_col > temp.red_col) {
+				
+				count=count+go_right(temp.blue_row, temp.blue_col, temp.dir);
+				push_temp.blue_row = push_row;
+				push_temp.blue_col = push_col;
+				count = count + go_right(temp.red_row, temp.red_col, temp.dir);
+				push_temp.red_row = push_row;
+				push_temp.red_col = push_col;
+				push_temp.dir = 1;
+			}
+			else {
+				count = count + go_right(temp.red_row, temp.red_col, temp.dir);
+				push_temp.red_row = push_row;
+				push_temp.red_col = push_col;
+				count = count + go_right(temp.blue_row, temp.blue_col, temp.dir);
+				push_temp.blue_row = push_row;
+				push_temp.blue_col = push_col;
+				push_temp.dir = 1;
+			}
+			break;
+		case 2://밑
+			if (temp.blue_row > temp.red_row) {
+				count = count + go_down(temp.blue_row, temp.blue_col, temp.dir);
+				push_temp.blue_row = push_row;
+				push_temp.blue_col = push_col;
+				count = count + go_down(temp.red_row, temp.red_col, temp.dir);
+				push_temp.red_row = push_row;
+				push_temp.red_col = push_col;
+				push_temp.dir = 1;
+			}
+			else {
+				count = count + go_down(temp.red_row, temp.red_col, temp.dir);
+				push_temp.red_row = push_row;
+				push_temp.red_col = push_col;
+				count = count + go_down(temp.blue_row, temp.blue_col, temp.dir);
+				push_temp.blue_row = push_row;
+				push_temp.blue_col = push_col;
+				push_temp.dir = 1;
+			}
+			break;
+		case 3://왼쪽
+			if (temp.blue_col < temp.red_col) {
+				count = count + go_left(temp.blue_row, temp.blue_col, temp.dir);
+				push_temp.blue_row = push_row;
+				push_temp.blue_col = push_col;
+				count = count + go_left(temp.red_row, temp.red_col, temp.dir);
+				push_temp.red_row = push_row;
+				push_temp.red_col = push_col;
+				push_temp.dir = 1;
+			}
+			else {
+				count = count + go_left(temp.red_row, temp.red_col, temp.dir);
+				push_temp.red_row = push_row;
+				push_temp.red_col = push_col;
+				count = count + go_left(temp.blue_row, temp.blue_col, temp.dir);
+				push_temp.blue_row = push_row;
+				push_temp.blue_col = push_col;
+				push_temp.dir = 1;
+			}
+			break;
+		case 4://위
+			if (temp.blue_row < temp.red_row) {
+				count = count + go_up(temp.blue_row, temp.blue_col, temp.dir);
+				push_temp.blue_row = push_row;
+				push_temp.blue_col = push_col;
+				count = count + go_up(temp.red_row, temp.red_col, temp.dir);
+				push_temp.red_row = push_row;
+				push_temp.red_col = push_col;
+				push_temp.dir = 1;
+			}
+			else {
+				count = count + go_up(temp.red_row, temp.red_col, temp.dir);
+				push_temp.red_row = push_row;
+				push_temp.red_col = push_col;
+				count = count + go_up(temp.blue_row, temp.blue_col, temp.dir);
+				push_temp.blue_row = push_row;
+				push_temp.blue_col = push_col;
+				push_temp.dir = 1;
+			}
+			break;
+		}
+		//
+		if (flag == 1) {
+			break;
+		}
+		if (count > 0) {
+			for (int i = 0; i < state_arr.size(); i++) {
+				if (state_arr[i].blue_row == push_temp.blue_row&&state_arr[i].blue_col == push_temp.blue_col&&state_arr[i].red_row == push_temp.red_row&&state_arr[i].red_col == push_temp.red_col) {
+					vector_find = 1;
+				}
+				else {
+					
+				}
+			}
+			if (vector_find == 0) {
+				state_arr.push_back(push_temp);
+			}
+		}
+
+
+		count = 0;
+		vector_find = 0;
+	}
+	//stack 사용
+	//빨강 파랑구슬 위치비교
+}
+
+int go_right(int row, int col, int dir) {
+	int i;
+	int j;
+
+	int count = 0;
+	//count 랑 vector 확인 후에 push
+
+	i = row;
+	j = col;
+	arr[i][j] = 0;
+	for (;j < col_siz;) {
+		
+		i = i + a[dir].row;
+		j = j + a[dir].col;
+		if (arr[i][j] != 0) {
+			break;
+		}
+		count++;
+	}
+	//범위넘어갈때
+	
+	//3일때
+	if (arr[i][j] == 3)
+		flag = 1; // 끝남;
+	else {
+		j = j - 1;
+	}
+	push_row = i;
+	push_col = j;
+	arr[i][j] = 2;
+	return count;
+	//벽에 부딪혓을때는 따로 안해도 될듯
+}
+int go_down(int row, int col, int dir) {
+	int i;
+	int j;
+	int count = 0;
+	i = row;
+	j = col;
+	arr[i][j] = 0;
+	for (; i < row_siz;) {
+		
+		i = i + a[dir].row;
+		j = j + a[dir].col;
+		if (arr[i][j] != 0) {
+			break;
+		}
+		count++;
+	}
+	//범위넘어갈때
+	if (arr[i][j] == 3)
+		flag = 1; // 끝남;
+	else {
+		i = i - 1;
+	}
+	push_row = i;
+	push_col = j;
+	arr[i][j] = 2;
+	return count;
+}
+int go_left(int row, int col, int dir) {
+	int i;
+	int j;
+	int count = 0;
+	i = row;
+	j = col;
+	arr[i][j] = 0;
+	for (; j >= 0;) {
+		
+		i = i + a[dir].row;
+		j = j + a[dir].col;
+		if (arr[i][j] != 0) {
+			break;
+		}
+		count++;
+	}
+	//범위넘어갈때
+	if (j < 0) {
+		j = 0;
+	}
+	//3일때
+	if (arr[i][j] == 3)
+		flag = 1; // 끝남;
+	else {
+		j = j+1;
+	}
+	push_row = i;
+	push_col = j;
+	arr[i][j] = 2;
+	return count;
+}
+int go_up(int row, int col, int dir) {
+	int i;
+	int j;
+	int count = 0;
+	i = row;
+	j = col;
+	arr[i][j] = 0;
+	for (; i >= 0;) {
+		
+		i = i + a[dir].row;
+		j = j + a[dir].col;
+		if (arr[i][j] != 0) {
+			break;
+		}
+		count++;
+	}
+	//범위넘어갈때
+	if (i < 0) {
+		i = 0;
+	}
+	//3일때
+	if (arr[i][j] == 3)
+		flag = 1; // 끝남;
+	else {
+		i = i + 1;
+	}
+	push_row = i;
+	push_col = j;
+	arr[i][j] = 2;
+	return count;
+}
+void initial_direction(){
+	a[1].row=0;
+	a[1].col=1;
+	a[2].row=1;
+	a[2].col=0;
+	a[3].row=0;
+	a[3].col=-1;
+	a[4].row=-1;
+	a[4].col=0;
+}
---
+++ baekjoon/17825. 주사위 윷놀이/진오/playing_yut.cpp
@@ -0,0 +1,132 @@
+﻿#include <iostream>
+using namespace std;
+#define endl '\n' 
+
+struct dice {
+	int dice_num;
+	int horse_num;
+};
+dice arr[10];
+//이동 규칙이 복잡할 수 있으므로 Look-up 테이블을 만들어서 사용한다.
+//jump[index][0] = 해당 판 점수 //jump[index][1~5] => 주사위 해당 수가 나오면 이동하는 양 
+int jump[33][6] = { {0,1,2,3,4,5},//0번자리
+{2,2,3,4,5,9}, //1번자리
+{4,3,4,5,9,10},//2번자리
+{6,4,5,9,10,11}, //3번자리
+{8,5,9,10,11,12},//4번자리
+{10,6,7,8,20,29},//5번자리 
+{13,7,8,20,29,30}, //6번자리 
+{16,8,20,29,30,31}, //7번자리
+{19,20,29,30,31,32}, //8번자리 
+{12,10,11,12,13,14}, //9번자리
+{14,11,12,13,14,15}, //10번자리 
+{16,12,13,14,15,16}, //11번자리
+{18,13,14,15,16,17}, //12번자리
+{20,18,19,20,29,30}, //13번자리
+{22,15,16,17,24,25}, //14번자리 
+{24,16,17,24,25,26}, //15번자리 
+{26,17,24,25,26,27}, //16번자리 
+{28,24,25,26,27,28}, //17번자리
+{22,19,20,29,30,31}, //18번자리
+{24,20,29,30,31,32}, //19번자리
+{25,29,30,31,32,32}, //20번자리 
+{26,20,29,30,31,32}, //21번자리
+{27,21,20,29,30,31}, //22번자리 
+{28,22,21,20,29,30}, //23번자리 
+{30,23,22,21,20,29}, //24번자리 
+{32,26,27,28,31,32}, //25번자리 
+{34,27,28,31,32,32}, //26번자리
+{36,28,31,32,32,32}, //27번자리 
+{38,31,32,32,32,32}, //28번자리 
+{30,30,31,32,32,32}, //29번자리
+{35,31,32,32,32,32}, //30번자리
+{40,32,32,32,32,32}, //31번자리
+{0,32,32,32,32,32} //32번자리 
+}; 
+int move();
+int simul();
+
+
+int main() {
+	for (int i = 0; i < 10; i++) {
+		cin >> arr[i].dice_num;
+	}
+	cout << simul();
+}
+int simul() {
+	int count = 0;
+	int temp;
+	for (int i0 = 1; i0 <= 4; i0++) {
+		arr[0].horse_num = i0;
+		for (int i1 = 1; i1 <= 4; i1++) {
+			arr[1].horse_num = i1;
+			for (int i2 = 1; i2 <= 4; i2++) {
+				arr[2].horse_num = i2;
+				for (int i3 = 1; i3 <= 4; i3++) {
+					arr[3].horse_num = i3;
+					for (int i4 = 1; i4 <= 4; i4++) {
+						arr[4].horse_num = i4;
+						for (int i5 = 1; i5 <= 4; i5++) {
+							arr[5].horse_num = i5;
+							for (int i6 = 1; i6 <= 4; i6++) {
+								arr[6].horse_num = i6;
+								for (int i7 = 1; i7 <= 4; i7++) {
+									arr[7].horse_num = i7;
+									for (int i8 = 1; i8 <= 4; i8++) {
+										arr[8].horse_num = i8;
+										for (int i9 = 1; i9 <= 4; i9++) {
+											arr[9].horse_num = i9;
+											temp = move();
+											if (temp > count) {
+												count = temp;
+											}
+										}
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+	return count;
+}
+int move() {
+	int visit[33] = { 0, };
+	int end[5] = { 0, };// 1~4번 끝났는지
+	int num = 0;
+	int sum = 0;
+	int temp;
+	int i;
+	while (1) {
+		if (num >= 10) {
+			break;
+		}
+		for (i = 1; i < 32; i++) {
+			if (arr[num].horse_num == visit[i]) {
+				temp = i + jump[i][arr[num].dice_num];
+				if (temp< 33) {
+					visit[temp] = arr[num].horse_num;
+					visit[i] = 0;
+					sum = sum + jump[temp][0];
+					break;
+				}
+				else if(visit[temp]!=0){
+					return -1;
+				}
+				else {
+					visit[i] = 0;
+					end[arr[num].horse_num] = 1;
+				}
+			}
+		}
+		if (i == 32 && end[arr[num].horse_num] != 1) { //
+			visit[jump[0][arr[num].dice_num]] = arr[num].horse_num;
+			sum = sum + jump[0][arr[num].dice_num];
+		}
+		num++;
+
+	}
+	return sum;
+}
