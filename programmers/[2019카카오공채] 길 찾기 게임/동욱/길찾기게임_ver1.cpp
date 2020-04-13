23f6ef7611b0a36612f0fa0553dee0cc2b5e2153
Dong-wook94 <gygacpu@naver.com>
2020-04-13 20:39:41 +0900

    길찾기 게임 malloc 사용 버전 동욱  풀이완료

---
+++ programmers/[2019카카오공채] 길 찾기 게임/동욱/길찾기게임_ver1.cpp
@@ -0,0 +1,89 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+typedef struct node *treePointer;
+typedef struct node {
+	int data;
+	int num;
+	treePointer leftChild, rightChild;
+}node;
+treePointer root;
+bool cmp(vector<int> a, vector<int> b) {
+	return a[1] > b[1];
+}
+vector<int> prevector;
+vector<int> postvector;
+void preorder(treePointer ptr) {
+	if (ptr) {
+		//printf("%d ", ptr->data);
+		prevector.push_back(ptr->num);
+		preorder(ptr->leftChild);
+		preorder(ptr->rightChild);
+	}
+}
+void postorder(treePointer ptr) {
+	if (ptr) {
+		postorder(ptr->leftChild);
+		postorder(ptr->rightChild);
+		postvector.push_back(ptr->num);
+	}
+}
+vector<vector<int>> solution(vector<vector<int>> nodeinfo) {
+	vector<vector<int>> answer;
+	treePointer temp;
+	root = (node*)malloc(sizeof(node));
+	for (int i = 0; i < nodeinfo.size(); i++) {
+		nodeinfo[i].push_back(i+1);
+		//cout << nodeinfo[i][2] << endl;
+	}
+	sort(nodeinfo.begin(), nodeinfo.end(), cmp);
+	root->data = nodeinfo[0][0];
+	root->num = nodeinfo[0][2];
+	root->rightChild = NULL;
+	root->leftChild = NULL;
+	for (int i = 1; i < nodeinfo.size(); i++) {
+		cout << nodeinfo[i][2] << endl;
+		temp = (node*)malloc(sizeof(node));
+		temp->data = nodeinfo[i][0];
+		temp->num = nodeinfo[i][2];
+		temp->rightChild = NULL;
+		temp->leftChild = NULL;
+		for (treePointer cur = root; ;) {
+			if (temp->data < cur->data) {
+				if (cur->leftChild) {
+					cur = cur->leftChild;
+				}
+				else {
+					cur->leftChild = temp;
+					break;
+				}
+
+			}
+			else {
+				if (cur->rightChild) {
+					cur = cur->rightChild;
+				}
+				else {
+					cur->rightChild = temp;
+					break;
+				}
+
+			}
+		}
+	}
+	preorder(root);
+	postorder(root);
+	answer.push_back(prevector);
+	answer.push_back(postvector);
+
+
+	return answer;
+}
+int main() {
+	vector<vector<int>> input = { {5, 3}, {11, 5}, {13, 3}, {3, 5}, {6, 1}, {1, 3}, {8, 6}, {7, 2}, {2, 2} };
+
+	solution(input);
+}
