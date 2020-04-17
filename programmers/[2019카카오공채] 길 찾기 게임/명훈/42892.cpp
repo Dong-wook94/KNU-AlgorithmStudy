954e5b6929077a67fb85773cc004a7a9aef666bb
Myunghun Park <mh97888@gmail.com>
2020-04-17 11:42:05 +0900

    week15 problem1 명훈

---
+++ programmers/[2019카카오공채] 길 찾기 게임/명훈/42892.cpp
@@ -0,0 +1,102 @@
+#include	<iostream>
+#include	<string>
+#include	<vector>
+#include	<algorithm>
+using namespace std;
+
+vector<int> pre_vector;
+vector<int> post_vector;
+
+class TreeNode {
+	friend class Tree;
+private:
+	TreeNode* leftChild;
+	TreeNode* rightChild;
+	int x;
+	int num;
+public:
+	TreeNode(int tx, int tnum) { x = tx; num = tnum; leftChild = NULL; rightChild = NULL; }
+};
+
+class Tree {
+private:
+	TreeNode* root;
+public:
+	Tree() : root(NULL) {};
+	~Tree() {};
+	TreeNode* getRoot() { return root; }
+	void insertNode(int x, int num) {
+		TreeNode* temp = new TreeNode(x, num);
+		if (root == NULL) {
+			root = temp;
+			return;
+		}
+		for (TreeNode* cur = root;;) {
+			if (temp->x < cur->x) {
+				if (cur->leftChild != NULL)
+					cur = cur->leftChild;
+				else {
+					cur->leftChild = temp;
+					return;
+				}
+			}
+			else {
+				if (cur->rightChild != NULL)
+					cur = cur->rightChild;
+				else {
+					cur->rightChild = temp;
+					return;
+				}
+			}
+		}
+	}
+	void preorder(TreeNode* cur) {
+		if (cur) {
+			pre_vector.push_back(cur->num);
+			preorder(cur->leftChild);
+			preorder(cur->rightChild);
+		}
+	}
+	void postorder(TreeNode* cur) {
+		if (cur) {
+			postorder(cur->leftChild);
+			postorder(cur->rightChild);
+			post_vector.push_back(cur->num);
+		}
+	}
+};
+
+bool cmp(vector<int> a, vector<int> b) {
+	return a[1] > b[1];
+}
+
+vector<vector<int>> solution(vector<vector<int>> nodeinfo) {
+	vector<vector<int>> answer;
+
+	for (int i = 0; i < nodeinfo.size(); i++)
+		nodeinfo[i].push_back(i + 1);
+	sort(nodeinfo.begin(), nodeinfo.end(), cmp);
+
+	Tree* b_tree = new Tree();
+	for (int i = 0; i < nodeinfo.size(); i++)
+		b_tree->insertNode(nodeinfo[i][0], nodeinfo[i][2]);
+
+	b_tree->preorder(b_tree->getRoot());
+	b_tree->postorder(b_tree->getRoot());
+	answer.push_back(pre_vector);
+	answer.push_back(post_vector);
+
+	return answer;
+}
+
+int main() {
+	vector<vector<int>> res = solution({ {5, 3}, {11, 5}, {13, 3}, {3, 5}, {6, 1}, {1, 3}, {8, 6}, {7, 2}, {2, 2} });
+
+	for (int i = 0; i < res.size(); i++) {
+		for (int j = 0; j < res[i].size(); j++)
+			cout << res[i][j] << " ";
+		cout << "\n";
+	}
+
+	return 0;
+}
